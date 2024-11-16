2d1f10378fcc00a07c1bfb773a18ca54b29cd0aa
Alfred <76535763+free20064u@users.noreply.github.com>
2024-11-16 14:56:39 -0800

    initial migrations run

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    middle_name = models.CharField(max_length=100, null=True, blank=True)
+    image = models.ImageField(default='profile/wbm-logo.png', upload_to='profile')
+    #program = models.ManyToManyField(Program, blank=True, null=True, default=1)
+    # add additional fields in here
+
+    def __str__(self):
+        return self.username

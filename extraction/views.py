7212b0dcd901c2bb32cfa5d3adb62f7d367ea087
Alfred <76535763+free20064u@users.noreply.github.com>
2024-11-25 11:22:00 -0800

    panda and numpy removed

--- extraction/views.py
+++ extraction/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .models import *
-import pandas as pd
 
 from main.models import Student
 
